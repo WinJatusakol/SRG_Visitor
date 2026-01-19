169c6e0c726884cfcd621da6548f75cc291c30b4
Kaivasas <folky2015@gmail.com>
2026-01-19 16:51:03 +0700

    Create login and Admin page to looking dashboard

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 
 export const createClient = async () => {
   const cookieStore = await cookies();
-  return createServerClient(
+return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     (process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ??
       process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY ??
